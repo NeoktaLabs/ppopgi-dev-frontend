04ed9555c3d10cd4c6fa5fd2f11ddeb317da79dc
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-01-18 21:18:29 +0100

    Update contracts.ts

--- ppopgi-frontend/src/config/contracts.ts
+++ ppopgi-frontend/src/config/contracts.ts
@@ -2,6 +2,12 @@ import LotteryRegistryAbi from "./abis/LotteryRegistry.json";
 import SingleWinnerDeployerAbi from "./abis/SingleWinnerDeployer.json";
 import LotterySingleWinnerAbi from "./abis/LotterySingleWinner.json";
 
+export const ADDRESSES = {
+  LotteryRegistry: "0x1CD24E0C49b1B61ff07be12fBa3ce58eCb20b098",
+  SingleWinnerDeployer: "0x6050196520e7010Aa39C8671055B674851E2426D",
+  USDC: "0x796Ea11Fa2dD751eD01b53C372fFDB4AAa8f00F9",
+} as const;
+
 export const ABIS = {
   LotteryRegistry: LotteryRegistryAbi,
   SingleWinnerDeployer: SingleWinnerDeployerAbi,
