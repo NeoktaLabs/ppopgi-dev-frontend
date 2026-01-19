459e99d27cff3e75854b2daefae9972eabb64ccb
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-01-19 09:35:13 +0100

    Update main.tsx

--- ppopgi-frontend/src/main.tsx
+++ ppopgi-frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+import { ThirdwebProvider } from "thirdweb/react";
+import { thirdwebClient } from "./thirdweb/client";
+import { ETHERLINK_CHAIN } from "./thirdweb/etherlink";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThirdwebProvider client={thirdwebClient} activeChain={ETHERLINK_CHAIN}>
+      <App />
+    </ThirdwebProvider>
+  </StrictMode>
+);
